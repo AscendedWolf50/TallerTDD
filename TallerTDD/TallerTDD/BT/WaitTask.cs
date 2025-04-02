cfb7f3f863c44ff2c1bb6e309c48e8c18b1e2d82
AscendedWolf50 <builesjuanjo10@gmail.com>
2025-04-02 14:35:58 -0500

    Archivos

---
+++ TallerTDD/TallerTDD/BT/WaitTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TallerBT.BT
+{
+    public class WaitTask : TaskNode
+    {
+        private float waitTime;
+        private bool completed = false;
+
+        public WaitTask(float time) => waitTime = time;
+
+        public override bool Execute()
+        {
+            if (completed) return true;
+
+            //Console.WriteLine($"[Pausa] Esperando {waitTime}s...");
+            System.Threading.Thread.Sleep((int)(waitTime * 1000));
+            completed = true;
+            return true;
+        }
+
+        public override void Reset()
+        {
+            completed = false;
+        }
+    }
+
+}
